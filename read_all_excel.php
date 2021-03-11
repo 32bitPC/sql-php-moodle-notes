22ddf61e6bb0fe0c94334f53b7650329a42a72ec
1967 Chevrolet Camaro SS <61229333+camaro-67@users.noreply.github.com>
2021-03-11 14:56:19 +0700

    Create read_all_excel.php

---
+++ read_all_excel.php
@@ -0,0 +1,182 @@
+<?php
+//Nhúng file PHPExcel
+require "PHPLibrary/PHPExcel/Classes/PHPExcel.php";
+
+//Đường dẫn file
+$file = 'excel_questions_1_1.xlsx';
+//Tiến hành xác thực file
+$objFile = PHPExcel_IOFactory::identify($file);
+$objData = PHPExcel_IOFactory::createReader($objFile);
+
+//Chỉ đọc dữ liệu
+$objData->setReadDataOnly(true);
+
+// Load dữ liệu sang dạng đối tượng
+$objPHPExcel = $objData->load($file);
+
+//Lấy ra số trang sử dụng phương thức getSheetCount();
+// Lấy Ra tên trang sử dụng getSheetNames();
+
+//Chọn trang cần truy xuất
+$sheet = $objPHPExcel->setActiveSheetIndex(0);
+
+//Lấy ra số dòng cuối cùng
+$Totalrow = $sheet->getHighestRow();
+//Lấy ra tên cột cuối cùng
+$LastColumn = $sheet->getHighestColumn();
+
+//Chuyển đổi tên cột đó về vị trí thứ, VD: C là 3,D là 4
+$TotalCol = PHPExcel_Cell::columnIndexFromString($LastColumn);
+
+//Tạo mảng chứa dữ liệu
+$data = [];
+
+//Tiến hành lặp qua từng ô dữ liệu
+//----Lặp dòng, Vì dòng đầu là tiêu đề cột nên chúng ta sẽ lặp giá trị từ dòng 2
+for ($i = 2; $i <= $Totalrow; $i++) {
+    //----Lặp cột
+    for ($j = 0; $j < $TotalCol; $j++) {
+        // Tiến hành lấy giá trị của từng ô đổ vào mảng
+        $data[$i - 2][$j] = $sheet->getCellByColumnAndRow($j, $i)->getValue();;
+    }
+}
+//Hiển thị mảng dữ liệu
+for($i=0;$i<=$Totalrow-2;$i++){
+    echo "Question ".($i+1)."\n";
+    print_r("Category : ".$data[$i][0]."\n");
+    print_r("Name : ".$data[$i][1]."\n");
+    print_r("Question text : ".$data[$i][2]."\n");
+}
+
+$file = 'excel_answers_1_1.xlsx';
+//Tiến hành xác thực file
+$objFile = PHPExcel_IOFactory::identify($file);
+$objData = PHPExcel_IOFactory::createReader($objFile);
+
+//Chỉ đọc dữ liệu
+$objData->setReadDataOnly(true);
+
+// Load dữ liệu sang dạng đối tượng
+$objPHPExcel = $objData->load($file);
+
+//Lấy ra số trang sử dụng phương thức getSheetCount();
+// Lấy Ra tên trang sử dụng getSheetNames();
+
+//Chọn trang cần truy xuất
+$sheet = $objPHPExcel->setActiveSheetIndex(0);
+
+//Lấy ra số dòng cuối cùng
+$Totalrow = $sheet->getHighestRow();
+//Lấy ra tên cột cuối cùng
+$LastColumn = $sheet->getHighestColumn();
+echo "Total row is ".$Totalrow."\n";
+//Chuyển đổi tên cột đó về vị trí thứ, VD: C là 3,D là 4
+$TotalCol = PHPExcel_Cell::columnIndexFromString($LastColumn);
+
+//Tạo mảng chứa dữ liệu
+$data = [];
+
+//Tiến hành lặp qua từng ô dữ liệu
+//----Lặp dòng, Vì dòng đầu là tiêu đề cột nên chúng ta sẽ lặp giá trị từ dòng 2
+for ($i = 2; $i <= $Totalrow; $i++) {
+    //----Lặp cột
+    for ($j = 0; $j < $TotalCol; $j++) {
+        // Tiến hành lấy giá trị của từng ô đổ vào mảng
+        $data[$i - 2][$j] = $sheet->getCellByColumnAndRow($j, $i)->getValue();;
+    }
+}
+for($i=0;$i<=$Totalrow-2;$i++){
+    echo "Answer ".($i+1)."\n";
+    print_r("Question ID : ".$data[$i][0]."\n");
+    print_r("Answer : ".$data[$i][1]."\n");
+    print_r("Fraction : ".$data[$i][2]."\n\n");
+}
+
+$file = 'excel_slots_1_1.xlsx';
+//Tiến hành xác thực file
+$objFile = PHPExcel_IOFactory::identify($file);
+$objData = PHPExcel_IOFactory::createReader($objFile);
+
+//Chỉ đọc dữ liệu
+$objData->setReadDataOnly(true);
+
+// Load dữ liệu sang dạng đối tượng
+$objPHPExcel = $objData->load($file);
+
+//Lấy ra số trang sử dụng phương thức getSheetCount();
+// Lấy Ra tên trang sử dụng getSheetNames();
+
+//Chọn trang cần truy xuất
+$sheet = $objPHPExcel->setActiveSheetIndex(0);
+
+//Lấy ra số dòng cuối cùng
+$Totalrow = $sheet->getHighestRow();
+//Lấy ra tên cột cuối cùng
+$LastColumn = $sheet->getHighestColumn();
+
+//Chuyển đổi tên cột đó về vị trí thứ, VD: C là 3,D là 4
+$TotalCol = PHPExcel_Cell::columnIndexFromString($LastColumn);
+
+//Tạo mảng chứa dữ liệu
+$data = [];
+
+//Tiến hành lặp qua từng ô dữ liệu
+//----Lặp dòng, Vì dòng đầu là tiêu đề cột nên chúng ta sẽ lặp giá trị từ dòng 2
+for ($i = 2; $i <= $Totalrow; $i++) {
+    //----Lặp cột
+    for ($j = 0; $j < $TotalCol; $j++) {
+        // Tiến hành lấy giá trị của từng ô đổ vào mảng
+        $data[$i - 2][$j] = $sheet->getCellByColumnAndRow($j, $i)->getValue();;
+    }
+}
+for($i=0;$i<=$Totalrow-2;$i++){
+    echo "Quiz slot ".($i+1)."\n";
+    print_r("Slot : ".$data[$i][0]."\n");
+    print_r("Quiz ID : ".$data[$i][1]."\n");
+    print_r("Question ID : ".$data[$i][2]."\n");
+    print_r("Max score : ".$data[$i][3]."\n\n");
+}
+
+$file = 'excel_qtype_1_1.xlsx';
+//Tiến hành xác thực file
+$objFile = PHPExcel_IOFactory::identify($file);
+$objData = PHPExcel_IOFactory::createReader($objFile);
+
+//Chỉ đọc dữ liệu
+$objData->setReadDataOnly(true);
+
+// Load dữ liệu sang dạng đối tượng
+$objPHPExcel = $objData->load($file);
+
+//Lấy ra số trang sử dụng phương thức getSheetCount();
+// Lấy Ra tên trang sử dụng getSheetNames();
+
+//Chọn trang cần truy xuất
+$sheet = $objPHPExcel->setActiveSheetIndex(0);
+
+//Lấy ra số dòng cuối cùng
+$Totalrow = $sheet->getHighestRow();
+//Lấy ra tên cột cuối cùng
+echo "Total row is ".$Totalrow."\n";
+$LastColumn = $sheet->getHighestColumn();
+
+//Chuyển đổi tên cột đó về vị trí thứ, VD: C là 3,D là 4
+$TotalCol = PHPExcel_Cell::columnIndexFromString($LastColumn);
+
+//Tạo mảng chứa dữ liệu
+$data = [];
+
+//Tiến hành lặp qua từng ô dữ liệu
+//----Lặp dòng, Vì dòng đầu là tiêu đề cột nên chúng ta sẽ lặp giá trị từ dòng 2
+for ($i = 2; $i <= $Totalrow; $i++) {
+    //----Lặp cột
+    for ($j = 0; $j < $TotalCol; $j++) {
+        // Tiến hành lấy giá trị của từng ô đổ vào mảng
+        $data[$i - 2][$j] = $sheet->getCellByColumnAndRow($j, $i)->getValue();;
+    }
+}
+
+for($i=0;$i<=$Totalrow-2;$i++){
+    echo "Qtype ".($i+1)."\n";
+    print_r("Qtype : ".$data[$i][0]."\n\n");
+}
