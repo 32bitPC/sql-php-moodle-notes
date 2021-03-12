2a080b73d061745f3c7ac7044281c805321d60f6
1967 Chevrolet Camaro SS <61229333+camaro-67@users.noreply.github.com>
2021-03-12 17:08:17 +0700

    Create course-info.sql

---
+++ course-info.sql
@@ -0,0 +1,7 @@
+SELECT TOP 1 * FROM dbo.mdl232x0_course ORDER BY id DESC;
+SELECT TOP 1 * FROM dbo.mdl232x0_enrol ORDER BY id DESC;
+SELECT TOP 1 * FROM dbo.mdl232x0_user ORDER BY id DESC;
+SELECT TOP 1 * FROM dbo.mdl232x0_cohort ORDER BY id DESC;
+SELECT TOP 2 * FROM dbo.mdl232x0_cohort_members ORDER BY id DESC;
+SELECT TOP 3 * FROM dbo.mdl232x0_context ORDER BY id DESC;
+SELECT TOP 1 * FROM dbo.mdl232x0_role_assignments ORDER BY id DESC;
