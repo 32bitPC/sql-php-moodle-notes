d42b3a600a730f62e0f962991b2e7e10f35af7d5
1967 Chevrolet Camaro SS <61229333+camaro-67@users.noreply.github.com>
2021-03-15 16:51:02 +0700

    Update course-info.sql

--- course-info.sql
+++ course-info.sql
@@ -5,3 +5,11 @@ SELECT TOP 1 * FROM dbo.mdl232x0_cohort ORDER BY id DESC;
 SELECT TOP 2 * FROM dbo.mdl232x0_cohort_members ORDER BY id DESC;
 SELECT TOP 3 * FROM dbo.mdl232x0_context ORDER BY id DESC;
 SELECT TOP 1 * FROM dbo.mdl232x0_role_assignments ORDER BY id DESC;
+
+SELECT * FROM dbo.mdl_course ORDER BY id DESC LIMIT 1;
+SELECT * FROM dbo.mdl_enrol ORDER BY id DESC LIMIT 1
+SELECT * FROM dbo.mdl_user ORDER BY id DESC LIMIT 1;
+SELECT * FROM dbo.mdl_cohort ORDER BY id DESC LIMIT 1;
+SELECT * FROM dbo.mdl_cohort_members ORDER BY id DESC LIMIT 2;
+SELECT * FROM dbo.mdl_context ORDER BY id DESC LIMIT 3;
+SELECT * FROM dbo.mdl_role_assignments ORDER BY id DESC LIMIT 1;
