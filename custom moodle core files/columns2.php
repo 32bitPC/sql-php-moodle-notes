5a27ffca642053d19b91f5e1c116766e6008230a
1967 Chevrolet Camaro SS <61229333+camaro-67@users.noreply.github.com>
2021-03-16 11:06:09 +0700

    Create columns2.php

---
+++ custom moodle core files/columns2.php
@@ -0,0 +1,15 @@
+ob_start();
+require 'upload_students.php';
+$content = ob_get_clean();
+$templatecontext = [
+    'sitename' => format_string($SITE->shortname, true, ['context' => context_course::instance(SITEID), "escape" => false]),
+    'output' => $OUTPUT,
+    'sidepreblocks' => $blockshtml,
+    'hasblocks' => $hasblocks,
+    'bodyattributes' => $bodyattributes,
+    'navdraweropen' => $navdraweropen,
+    'regionmainsettingsmenu' => $regionmainsettingsmenu,
+    'hasregionmainsettingsmenu' => !empty($regionmainsettingsmenu),
+    'myCustom' => $content
+];
+echo $OUTPUT->render_from_template('theme_boost/columns2', $templatecontext);
