f7a293a16692658123543d98bbb5599eed6fac83
1967 Chevrolet Camaro SS <61229333+camaro-67@users.noreply.github.com>
2021-03-16 11:07:08 +0700

    Create upload_students.php

---
+++ custom moodle core files/upload_students.php
@@ -0,0 +1,16 @@
+<?php
+echo '
+<li>
+                        <a class="list-group-item list-group-item-action  " href="http://localhost:8083/moodle/upload_file/" data-key="privatefiles" data-isexpandable="0" data-indent="0" data-showdivider="0" data-type="1" data-nodetype="1" data-collapse="0" data-forceopen="1" data-isactive="0" data-hidden="0" data-preceedwithhr="0">
+                            <div class="ml-0">
+                                <div class="media">
+                                        <span class="media-left">
+                                            <i class="icon fa fa-file-o fa-fw " aria-hidden="true"></i>
+                                        </span>
+                                    <span class="media-body ">
+                                 Upload student records</span>
+                                </div>
+                            </div>
+                        </a>
+                    </li>';
+                    ?>
